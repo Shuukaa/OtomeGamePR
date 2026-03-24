be29adb3848dc1ed059e0a6d2b98b5d2d61741ca
Shuukaa <69614231+Shuukaa@users.noreply.github.com>
2026-03-23 23:04:35 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    localStoragePrefix: "party-rank-sorter",
+    title: "Otome Game Party Rank Sorter",
+    description: "Otome Game Party rank sorter for your list of songs."
+};
